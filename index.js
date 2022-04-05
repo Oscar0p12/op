a98db6c00aec63e14e000fb1cc3c84f43d7621d2
Oscar0p12 <oscar12pv@gmail.com>
2022-04-04 23:26:16 -0400

    tercer

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ const express=require('express')
 const cors= require('cors')
 
 //Crear el servidor
-const app=express()
-const {Server, Socket}=require('socket.io')
-
-const fs=require('fs')
-const archivo='../archivo.txt'
-const datos='datos.txt'
